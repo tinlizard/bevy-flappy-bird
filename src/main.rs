b06114a8d75e3e5e0340c685403c9bbd4a405dc8
benwcunicz <72759010@fsv.cuni.cz>
2024-05-04 20:21:59 +0200

    only animate bird on space key pressed

--- src/main.rs
+++ src/main.rs
@@ -82,16 +82,18 @@ fn key_input(keys: Res<ButtonInput<KeyCode>>, mut rect_pos: Query<&mut Transform
     }
 }
 
-fn animate_sprite(time: Res<Time>, mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlas)>){
-    for (indices, mut timer, mut atlas) in &mut query{
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            atlas.index = if atlas.index == indices.last {
-                indices.first
-            } 
-            else {
-                atlas.index + 1
-            };
+fn animate_sprite(keys: Res<ButtonInput<KeyCode>>, time: Res<Time>, mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlas)>){
+    if keys.pressed(KeyCode::Space){
+        for (indices, mut timer, mut atlas) in &mut query{
+            timer.tick(time.delta());
+            if timer.just_finished() {
+                atlas.index = if atlas.index == indices.last {
+                    indices.first
+                } 
+                else {
+                    atlas.index + 1
+                };
+            }
         }
     }
 }
